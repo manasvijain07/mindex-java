ff94282b46c60982887db8e186fd0f375ea205d8
manasvijain07 <46429718+manasvijain07@users.noreply.github.com>
2021-04-11 23:38:43 -0700

    new files

--- src/main/java/com/mindex/challenge/dao/CompensationRepository.java
+++ src/main/java/com/mindex/challenge/dao/CompensationRepository.java
@@ -4,10 +4,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mindex.challenge.data.Compensation;
-import com.mindex.challenge.data.Employee;
 
 @Repository
 public interface CompensationRepository extends MongoRepository<Compensation, String>  {
-	Compensation findByEmployee(Employee employee);
+	Compensation findByEmployeeEmployeeId(String employeeId);
 
 }
--- src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -14,17 +14,47 @@ import org.springframework.stereotype.Service;
 
 import com.mindex.challenge.dao.CompensationRepository;
 import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.service.CompensationService;
 
 @Service
-public class CompensationServiceImpl {
+public class CompensationServiceImpl implements CompensationService{
 	
 	private static final Logger LOG = LoggerFactory.getLogger(CompensationServiceImpl.class);
 
     @Autowired
     private CompensationRepository compensationRepo;
     
-    
-    
-    
+    @Autowired
+    private EmployeeRepository empRepo;
+
+	@Override
+	public Compensation createCompensation(Compensation compensation) {
+		String emp_id = compensation.getEmployee().getEmployeeId();
+		if(empRepo.findByEmployeeId(emp_id) != null)
+		{
+			LOG.debug("Creating compensation for [{}]", compensation.getEmployee());
+			compensationRepo.insert(compensation);
+		}
+		else
+		{
+			LOG.debug("No such employee found. Please check employee details");
+		}
+		return compensation;
+	}
+
+	@Override
+	public Compensation readCompensation(String id) {
+		Compensation comp = null;
+		try
+		{
+			comp = compensationRepo.findByEmployeeEmployeeId(id);
+		}
+		catch(NullPointerException e)
+		{
+			LOG.debug("No compensation found for [{}]", id);
+		}
+		return comp;	
+	}
 
 }
